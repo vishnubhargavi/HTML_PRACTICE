b9e87d3d1525cb5dc3bf2daec0fe47dbfb94718f
vishnubhargavi <vishnubhargavi6912@gmail.com>
2023-03-29 16:05:05 -0400

    commit

--- JS/Pokemon/pokemon.js
+++ JS/Pokemon/pokemon.js
@@ -4,9 +4,15 @@ const container = document.querySelector('#container');
 const baseurl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
 
-for(let i=1;i<151;i++) {
+for(let i=1;i<=151;i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('sprites');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`       
+    const newimg = document.createElement('img');
+    newimg.src = `${baseurl}${i}.png`;
 
-const newimg = document.createElement('img');
-newimg.src = `${baseurl}${i}.png`;
-container.appendChild(newimg);
+    pokemon.appendChild(newimg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
 }
